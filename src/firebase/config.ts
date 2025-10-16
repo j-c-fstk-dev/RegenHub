b689e825ccfa6ccb9b70afb03663b20d47b49d1a
Beregenerative <beregenerative.life@gmail.com>
2025-10-16 03:49:52 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:554748399159:web:f98b601a140ba2d91ef9b2",
   "apiKey": "AIzaSyABRjr8ZRUyO2D7uPSWUBb0K_B2QL_1AIc",
   "authDomain": "studio-2333981044-2390d.firebaseapp.com",
+  "storageBucket": "studio-2333981044-2390d.appspot.com",
   "measurementId": "",
   "messagingSenderId": "554748399159"
 };
