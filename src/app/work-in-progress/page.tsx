11d845e8e6be4f3e3d696e1ed56455b376877c62
Beregenerative <beregenerative.life@gmail.com>
2025-12-06 05:37:54 +0000

    The Netlify deploy errored, with the following guidance provided:
    Diagn

--- src/app/work-in-progress/page.tsx
+++ src/app/work-in-progress/page.tsx
@@ -1,6 +1,6 @@
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Wrench } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
